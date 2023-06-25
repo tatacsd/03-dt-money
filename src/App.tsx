eef889376667e5eb181530f4a731d48718631b18
thayscasado <thayscasado@hotmail.com>
2023-06-24 17:34:35 -0700

    Added styled-components setting to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Vite + React</h1>
-    </>
+    </ThemeProvider>
   );
 }
