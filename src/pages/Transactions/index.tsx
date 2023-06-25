d0b10792a3578207ffcd15405d67ec05700e9e85
thayscasado <thayscasado@hotmail.com>
2023-06-24 20:02:06 -0700

    🎨 Added Tabela with transactions to transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Pay check</td>
+              <td>
+                <PriceHighlight variant="income">3.000,00</PriceHighlight>
+              </td>
+              <td>income</td>
+              <td>20/02/2023</td>
+            </tr>
+            <tr>
+              <td>Rent</td>
+              <td>
+                <PriceHighlight variant="outcome">1.000,00</PriceHighlight>
+              </td>
+              <td>Fixed</td>
+              <td>20/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
